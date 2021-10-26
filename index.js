1a1269232f6d33f742ada690e73c014bc759505a
OmieSawie <omkarsawant20HG@gmail.com>
2021-10-27 03:15:28 +0530

    switch to kontests API

--- index.js
+++ index.js
@@ -14,29 +14,29 @@ client.once('ready', () => {
 });
 
 async function content_list()
-{	const response = await fetch("https://codeforces.com/api/contest.list?gym=false") ;
+{	const response = await fetch("https://kontests.net/api/v1/all") ;
                 const listReply = await response.json() ;
 		console.log("Data fetched") ;
 		return listReply ;
 }
 
 
-function unixToIST(i){
-        let unix_timestamp = JSON.stringify(listReply.result[i]['startTimeSeconds'],null,2) ;
- 
-                  // Create a new JavaScript Date object based on the timestamp
-                  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-                  let date = new Date(unix_timestamp * 1000);
-                  // Hours part from the timestamp
-                  let hours = date.getHours();
-                  // Minutes part from the timestamp
-                  let minutes = "0" + date.getMinutes();
-                  // Seconds part from the timestamp
-                  let seconds = "0" + date.getSeconds();
-                  // Will display time in 10:30:23 format
-                  let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-                    return date ;
-}
+//function unixToIST(i){
+//        let unix_timestamp = JSON.stringify(listReply.result[i]['startTimeSeconds'],null,2) ;
+// 
+//                  // Create a new JavaScript Date object based on the timestamp
+//                  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+//                  let date = new Date(unix_timestamp * 1000);
+//                  // Hours part from the timestamp
+//                  let hours = date.getHours();
+//                  // Minutes part from the timestamp
+//                  let minutes = "0" + date.getMinutes();
+//                  // Seconds part from the timestamp
+//                  let seconds = "0" + date.getSeconds();
+//                  // Will display time in 10:30:23 format
+//                  let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+//                    return date ;
+//}
 
 
 client.on('interactionCreate', async interaction => {
@@ -59,29 +59,29 @@ client.on('interactionCreate', async interaction => {
 	else if (commandName === 'contest_list') {
             await interaction.deferReply();
 		listReply = await content_list() ;
-            console.log(listReply.result.length);
+            //console.log(listReply.result.length);
             //contentList.then(data => console.log(data))
             
             //for(let i=0; i<10; i++){
             //    let contestListObject = JSON.stringify(listReply.result[i],null,2)
             //
-                let unix_timestamp = JSON.stringify(listReply.result[1]['startTimeSeconds'],null,2) ;
+            //    let unix_timestamp = JSON.stringify(listReply.result[1]['startTimeSeconds'],null,2) ;
                 
                 // Create a new JavaScript Date object based on the timestamp
                 // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-                let date = new Date(unix_timestamp * 1000);
+            //    let date = new Date(unix_timestamp * 1000);
                 // Hours part from the timestamp
-                let hours = date.getHours();
+            //    let hours = date.getHours();
                 // Minutes part from the timestamp
-                let minutes = "0" + date.getMinutes();
+            //    let minutes = "0" + date.getMinutes();
                 // Seconds part from the timestamp
-                let seconds = "0" + date.getSeconds();
+            //    let seconds = "0" + date.getSeconds();
                 // Will display time in 10:30:23 format
-                let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            //    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
               
 
-        console.log(formattedTime);   
-            await interaction.editReply(`The contest list is as follows:\n\n\n ${JSON.stringify(listReply.result[0]['id','name'], null, 2)} \n date: ${unixToIST(0)}    \n\n\n   \n ${JSON.stringify(listReply.result[1]['id','name'], null, 2)} \n date: ${unixToIST(1)}      \n\n\n  \n${JSON.stringify(listReply.result[2]['id','name'], null, 2)} \n date: ${unixToIST(2)}  \n\n\n  \n${JSON.stringify(listReply.result[3]['id','name'], null, 2)} \n date: ${unixToIST(3)}  \n\n\n  \n${JSON.stringify(listReply.result[4]['id','name'], null, 2)} \n date: ${unixToIST(4)}
+        //console.log(formattedTime);   
+            await interaction.editReply(`The contest list is as follows:\n\n\n ${JSON.stringify(listReply.result[0], null, 2)} \n\n\n\n   \n ${JSON.stringify(listReply.result[1], null, 2)} \n\n\n\n  \n${JSON.stringify(listReply.result[2], null, 2)} \n\n\n\n  \n${JSON.stringify(listReply.result[3], null, 2)} \n\n\n\n  \n${JSON.stringify(listReply.result[4], null, 2)} \n
                 `) ;
         }
 });
