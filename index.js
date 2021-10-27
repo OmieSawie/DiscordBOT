35a616e34c5161c4ce03123364b265d1cbd04556
OmieSawie <omkarsawant20HG@gmail.com>
2021-10-27 19:45:59 +0530

    Komtest API with date and time

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
-const _ = require('lodash');
+
 
 const fetch = require('node-fetch');
+const { delay } = require('lodash');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -15,28 +16,37 @@ client.once('ready', () => {
 
 async function content_list()
 {	const response = await fetch("https://kontests.net/api/v1/all") ;
-                const listReply = await response.json() ;
+                const JSONlistReply = await response.json() ;
 		console.log("Data fetched") ;
-		return listReply ;
+
+        var datetime = new Date();
+        const listReply = {};
+        console.log(datetime) ;
+
+        var timeNow = new Date()
+        timeNow = timeNow.toDateString()
+        
+                console.log(`\n Time Now: ${timeNow}`)
+        
+                const trialTime = '2021-08-19T04:00:00.000Z'
+        
+                dayNow = Number(trialTime.substr(8,2)) 
+                monthNow = Number(trialTime.substr(5,2))
+                yearNow = Number(trialTime.substr(0,4))
+
+        console.log(`length of array= ${listReply.length}`) ;
+
+        
+        // var duration = contestTime.diff(timeNow);
+        // console.log(`Time difference= ${duration}`)
+
+        // while(JSONlistReply[i].start_time.diff(datetime).hours() <= 96){
+
+        // for()
+        
+		return JSONlistReply ;
 }
 
-
-//function unixToIST(i){
-//        let unix_timestamp = JSON.stringify(listReply.result[i]['startTimeSeconds'],null,2) ;
-// 
-//                  // Create a new JavaScript Date object based on the timestamp
-//                  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-//                  let date = new Date(unix_timestamp * 1000);
-//                  // Hours part from the timestamp
-//                  let hours = date.getHours();
-//                  // Minutes part from the timestamp
-//                  let minutes = "0" + date.getMinutes();
-//                  // Seconds part from the timestamp
-//                  let seconds = "0" + date.getSeconds();
-//                  // Will display time in 10:30:23 format
-//                  let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-//                    return date ;
-//}
 
 
 client.on('interactionCreate', async interaction => {
@@ -60,29 +70,35 @@ client.on('interactionCreate', async interaction => {
             await interaction.deferReply();
 		listReply = await content_list() ;
             //console.log(listReply.result.length);
-            //contentList.then(data => console.log(data))
-            
-            //for(let i=0; i<10; i++){
-            //    let contestListObject = JSON.stringify(listReply.result[i],null,2)
-            //
-            //    let unix_timestamp = JSON.stringify(listReply.result[1]['startTimeSeconds'],null,2) ;
-                
-                // Create a new JavaScript Date object based on the timestamp
-                // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-            //    let date = new Date(unix_timestamp * 1000);
-                // Hours part from the timestamp
-            //    let hours = date.getHours();
-                // Minutes part from the timestamp
-            //    let minutes = "0" + date.getMinutes();
-                // Seconds part from the timestamp
-            //    let seconds = "0" + date.getSeconds();
-                // Will display time in 10:30:23 format
-            //    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-              
+  
+        
+        const reply = listReply[1] ;
+       console.log("your reply",reply) ;
+       const bds=reply;
+       for(keys in reply) console.log("keys",keys)
+       console.log("ok",bds)
+       var datetime = new Date();
+        // await interaction.editReply(`your reply is : ${JSON.stringify(bds,['name', 'start_time', 'duration', 'site', 'url'],2)}  \n also, ${reply.start_time.diff(datetime).hours()}`) ;
 
-        //console.log(formattedTime);   
-            await interaction.editReply(`The contest list is as follows:\n\n\n ${JSON.stringify(listReply.result[0], null, 2)} \n\n\n\n   \n ${JSON.stringify(listReply.result[1], null, 2)} \n\n\n\n  \n${JSON.stringify(listReply.result[2], null, 2)} \n\n\n\n  \n${JSON.stringify(listReply.result[3], null, 2)} \n\n\n\n  \n${JSON.stringify(listReply.result[4], null, 2)} \n
-                `) ;
+        var contestTime = (reply.start_time) ;
+        console.log(`ContestTime = ${contestTime}`)
+
+        reply.start_time[10] = ' ' ;
+        console.log(reply.start_time[10]) ;
+
+        var timeInMss = Date.now()
+        console.log(`\n Time diff: ${timeInMss}`)
+
+    
+
+        String.prototype.replaceAt = function(index, replacement) {
+            return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+        }
+
+        
+
+        await interaction.editReply(`Contest Nsme: ${(bds.name)}  \n\n  URL: ${(bds.url)}  \n\n  Start Time : ${JSON.stringify(reply.start_time).replaceAt(11,' ')}`) ;
+     
         }
 });
 
--- script.js
+++ script.js
@@ -3,8 +3,31 @@
 
 // })
 
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-fetch('https://codeforces.com/api/contest.list?gym=true')
-	.then((res) => res.json())
-	.then(res => console.log(res));
+// fetch('https://codeforces.com/api/contest.list?gym=true')
+// 	.then((res) => res.json())
+// 	.then(res => console.log(res));
+
+// console.log(Date.now())
+
+// var datetime = new Date();
+// console.log(datetime);
+
+// var moment = require('moment')
+// time = 
+// console.log(moment()) ;
+
+
+var timeInMss = new Date()
+timeInMss = timeInMss.toDateString()
+
+        console.log(`\n Time diff: ${timeInMss}`)
+
+		const trialTime = '2021-08-19T04:00:00.000Z'
+
+		day = Number(trialTime.substr(8,2)) 
+		month = Number(trialTime.substr(5,2))
+		year = Number(trialTime.substr(0,4))
+
+		// console.log(`${}`)
